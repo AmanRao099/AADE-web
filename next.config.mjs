4dad654118acecf119aef4eafa9b1c51f023fb15
AinzeXRonin <amanrao099@gmail.com>
2024-11-07 21:50:12 +0530

    Base packages

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+    domains: ["drive.google.com","images.unsplash.com","firebasestorage.googleapis.com"],
+  },
+};
+
+export default nextConfig;
